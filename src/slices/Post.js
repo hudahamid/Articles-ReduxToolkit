a01376e08bcb9a6d4b56f459b56c928b49d41f8c
hudahamid <huda.00091581@gmail.com>
2023-03-18 16:31:32 +0300

    redux toolkit

---
+++ src/slices/Post.js
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const initialState = {
+    loading: false,
+    hasErrors: false,
+    post: {},
+  }
+  
+const postSlice = createSlice({
+    name: 'post',
+    initialState,
+    reducers: {
+      getPost: state => {  state.loading = true
+      
+      },
+      getPostSuccess: (state, { payload }) => {
+        state.post = payload
+        state.loading = false
+        state.hasErrors = false
+      },
+      getPostFailure: state => {
+        state.loading = false
+        state.hasErrors = true
+      },
+    },
+  })
+  export const { getPost, getPostSuccess, getPostFailure } = postSlice.actions
+
+
+
+
+  export const postSelector = state => state.post
+export default postSlice.reducer
+
+
+
+export function fetchPost(id) {
+    return async dispatch => {
+      dispatch(getPost())
+  
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${id}`
+        )
+        const data = await response.json()
+  
+        dispatch(getPostSuccess(data))
+      } catch (error) {
+        dispatch(getPostFailure())
+      }
+    }
+  }
+  
